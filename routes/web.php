0565713d74f6b04bcd971931dd95f231308e9e83
jerry2012kk <jerry2012kk@gmail.com>
2022-09-21 09:07:13 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('users', 'App\Http\Controllers\UsersController');
+
